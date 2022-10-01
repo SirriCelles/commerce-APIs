de818da1e54810c265ea9e56775ab6226ec8e037
SirriCelles <42035795+SirriCelles@users.noreply.github.com>
2022-10-01 01:29:52 +0100

    complete signup with login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,31 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsyc');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+  // User cannot input a role
+  const user = await User.create({
+    fullname: req.body.fullname,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    confirmedPassword: req.body.confirmedPassword,
+    address: req.body.address,
+    DoB: req.body.DoB,
+  });
+
+  const expiresIn = process.env.JWT_EXPIRES_IN;
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: expiresIn,
+  });
   res.status(201).json({
     status: 'success',
     data: {
       user,
+      token: {
+        accessToken: token,
+        expiresIn: expiresIn,
+      },
     },
   });
 });
